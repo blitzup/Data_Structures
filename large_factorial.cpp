385833b6936bdb8d1a6ac34b47abcc0179ae862f
blitzup <8blitzup@gmail.com>
2020-12-22 16:29:59 +0530

    Factorial of a large number using array (C++ short code)

--- large_factorial.cpp
+++ large_factorial.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <string>
-#include <algorithm>
-#define MAX 5000
+#define max 5000
 using namespace std;
 
-int multiply(int a[], int a_size, int m)
+int multiply(int a[], int length, int m)
 {
-    int mult = 0, carry = 0, length = 0;
-    int i;
-    for (i = 0; i < a_size; i++)
+    int final_length = 0, mult = 0, carry = 0, i;
+    for (i = 0; i < length; i++)
     {
-        mult = a[i] * m + carry;
-        carry = mult / 10;
+        mult = a[i]*m + carry;
         a[i] = mult % 10;
-        length++;
+        carry = mult / 10;
+        final_length++;
     }
-    
-    
+
     while(carry)
     {
         a[i] = carry % 10;
         carry = carry / 10;
-        length++;
+        final_length++;
         i++;
     }
 
-    return length;
+    return final_length;
 }
 
 int main()
@@ -38,25 +32,16 @@ int main()
     {
         int n;
         cin >> n;
-        if (n == 1 || n == 0)
-            cout << 1 << " ";
+        int a[max];
+        a[0] = 1;
+        int length = 1;
+        for (auto i = 2; i <= n; i++)
+            length = multiply(a, length, i);
 
-        else
-        {
-
-            int a[MAX];
-            a[0] = 1;
-            int length = 1;
-            for (auto i = 2; i <= n; i++)
-            {
-                length = multiply(a, length, i);
-            }
-
-            for (auto i = length - 1; i >= 0; i--)
-                cout << a[i];
-        }
-
+        for (auto i = length - 1; i >= 0; i--)
+            cout << a[i];
         cout << endl;
     }
+
     return 0;
 }
